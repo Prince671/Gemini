fdba96fa68c925977286774f0e13ce487d8d1bbd
Prince soni <157681120+Prince671@users.noreply.github.com>
2025-03-08 22:59:52 +0530

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,34 +1,44 @@
-const {
-  GoogleGenerativeAI,
-  HarmCategory,
-  HarmBlockThreshold,
-} = require("@google/generative-ai");
+// const apikey = "AIzaSyD7dpuG-cslWOb3tzYbsIH8_Egzya10lP4";
 
-// Correctly use process.env to access environment variables
-const apiKey = process.env.AIzaSyD5E9TDngU5XFCSm6BafZ2QdAD_3iwf5as;
-const genAI = new GoogleGenerativeAI(apiKey);
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ */
 
-const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash",
-});
-
-const generationConfig = {
-  temperature: 1,
-  topP: 0.95,
-  topK: 40,
-  maxOutputTokens: 8192,
-  responseMimeType: "text/plain",
-};
-
-async function run(prompt) {
-  const chatSession = model.startChat({
-    generationConfig,
-    history: [],
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai"
+  
+  const apiKey = "AIzaSyD5E9TDngU5XFCSm6BafZ2QdAD_3iwf5as";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
   });
-
-  const result = await chatSession.sendMessage(prompt);
-  console.log(result.response.text());
-  return result.response.text();
-}
-
-module.exports = run;
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+   // safetySettings: Adjust safety settings
+   // See https://ai.google.dev/gemini-api/docs/safety-settings
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+    return result.response.text();
+  }
+  
+  export default run
